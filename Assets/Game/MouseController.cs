479b068890dec3ba687db6207fadb1d99b248d9d
antek1085 <antek1085@o2.pl>
2024-12-05 16:36:23 +0100

    CameraRay

--- Assets/Game/MouseController.cs
+++ Assets/Game/MouseController.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
+    Vector3 mousePosition;
+    [SerializeField] LayerMask layerMask;
+
+    GameObject ship;
+    public Plane plane;
+    Ray ray;
     void Update()
-    { 
-        Camera.main.ScreenPointToRay(transform.position);
+    {
+        mousePosition = Input.mousePosition;
+        ray = Camera.main.ScreenPointToRay(mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        { 
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            RaycastHit hit;
+            
+            if(Physics.Raycast( ray, out hit, Mathf.Infinity, layerMask))
+            {
+                ship = hit.transform.gameObject;
+                Debug.Log(ship);
+            }
+        }
+
+        if (ship != null)
+        {
+            if (plane.Raycast(ray, out float distance))
+            {
+                ship.transform.position = ray.GetPoint(distance);
+
+                var transformPosition = ship.transform.position;
+                transformPosition.y = 16.5f;
+            }   
+        }
     }
 }
